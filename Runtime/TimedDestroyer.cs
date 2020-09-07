020cd899e30b8f4df194e0941d5ccdd9a8c00232
Thomas <thsbrown1@gmail.com>
2020-09-07 14:55:31 -0700

    Add TimedDestroyer capable of destroying after period of time.

---
+++ Runtime/TimedDestroyer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace StudioName.Runtime
+{
+    /// <summary>
+    /// Destroys gameobjects after a given amount of item
+    /// </summary>
+    public class TimedDestroyer : MonoBehaviour
+    {
+        /// <summary>
+        /// The time from our gameobjects creation till when it will be destroyed.
+        /// </summary>
+        [Tooltip("The time from our gameobjects creation till when it will be destroyed.")]
+        public float timeTillDestroy;
+        
+        private async void Awake()
+        {
+            await Task.Delay(TimeSpan.FromSeconds(timeTillDestroy));
+            Destroy(gameObject);
+        }
+    }
+}
