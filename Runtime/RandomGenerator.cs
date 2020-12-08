c5873049228dc404fb17a367bfb9966a68f97610
Thomas <thsbrown1@gmail.com>
2020-12-08 15:14:43 -0800

    Add ability to fast forward to random number generator

--- Runtime/RandomGenerator.cs
+++ Runtime/RandomGenerator.cs
@@ -9,7 +9,8 @@ namespace StudioName.Runtime {
     public class RandomGenerator {
         private Random random;
         private int seed = 0;
-
+        private int numbersGeneratedCount;
+        
         /// <summary>
         /// Generates a random number seeded from the system timer.
         /// </summary>
@@ -22,9 +23,13 @@ namespace StudioName.Runtime {
         /// Generates a random number based on the seed given
         /// </summary>
         /// <param name="seed">The int from which to seed from</param>
-        public RandomGenerator(int seed) {
+        /// <param name="fastForwardCount">The amount of generated numbers we should skip forward</param>
+        public RandomGenerator(int seed, int fastForwardCount = 0) {
             random = new Random(seed);
             this.seed = seed;
+            for (var i = 0; i < fastForwardCount; i++) {
+                random.Next(0);
+            }
         }
 
         /// <summary>
@@ -32,6 +37,7 @@ namespace StudioName.Runtime {
         /// </summary>
         /// <returns>Random integer generated</returns>
         public int Next() {
+            numbersGeneratedCount++;
             return random.Next();
         }
 
@@ -41,6 +47,7 @@ namespace StudioName.Runtime {
         /// <param name="maxValue">The max value (exclusive)</param>
         /// <returns>Random integer generated</returns>
         public int Next( int maxValue) {
+            numbersGeneratedCount++;
             return random.Next(maxValue);
         }
 
@@ -49,6 +56,7 @@ namespace StudioName.Runtime {
         /// </summary>
         /// <returns>Random double generated</returns>
         public double NextDouble() {
+            numbersGeneratedCount++;
             return random.NextDouble();
         }
 
@@ -59,6 +67,7 @@ namespace StudioName.Runtime {
         /// <param name="max">The max value (exclusive)</param>
         /// <returns>Random integer generated</returns>
         public int Range(int min, int max) {
+            numbersGeneratedCount++;
             return random.Next(min, max);
         }
 
@@ -69,6 +78,7 @@ namespace StudioName.Runtime {
         /// <param name="max">The max value (exclusive)</param>
         /// <returns></returns>
         public float Range( float min, float max) {
+            numbersGeneratedCount++;
             return (float)random.NextDouble() * (max - min) + min;
         }
 
@@ -79,5 +89,10 @@ namespace StudioName.Runtime {
             set { seed = value; random = new Random(value); }
             get { return seed; }
         }
+
+        /// <summary>
+        /// The total times the number generator has been used to create a random number
+        /// </summary>
+        public int NumbersGeneratedCount => numbersGeneratedCount;
     }
 }
