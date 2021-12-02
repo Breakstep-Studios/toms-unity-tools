264dfa4de27f821f4678589316a89245e634d27f
Thomas <thsbrown1@gmail.com>
2021-12-02 22:54:54 +0000

    Add enum extension

---
+++ Runtime/ExtensionsAndHelpers/EnumExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StudioName.Runtime.ExtensionAndHelpers
+{
+    /// <summary>
+    /// Extends the <see cref="Enum"/> class with additional functionality
+    /// </summary>
+    public static class EnumExtensions
+    {
+        /// <summary>
+        /// Get all the unique flags from an enum
+        /// </summary>
+        /// <param name="flags">The enum with flags</param>
+        /// <typeparam name="T">THe enums type</typeparam>
+        /// <returns>The enum values that have been set</returns>
+        public static IEnumerable<T> GetUniqueFlags<T>(this T flags) where T : Enum
+        {
+            foreach (Enum value in Enum.GetValues(flags.GetType()))
+            {
+                if (flags.HasFlag(value))
+                {
+                    yield return (T)value;
+                }
+            }
+        }
+    }
+}
