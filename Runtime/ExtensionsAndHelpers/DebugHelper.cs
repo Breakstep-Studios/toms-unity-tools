5d9e875b129229f971c71221dbe3b45282042f60
Thomas <thsbrown1@gmail.com>
2022-02-17 14:26:35 -0800

    Add makeStar paramater to DrawPoint function in DubugHelper
    - Add DrawBounds function to DebugHelper class.

--- Runtime/ExtensionsAndHelpers/DebugHelper.cs
+++ Runtime/ExtensionsAndHelpers/DebugHelper.cs
@@ -1,10 +1,10 @@
-﻿using UnityEngine;
+using UnityEngine;
 
 namespace StudioName.Runtime.ExtensionAndHelpers {
     /// <summary>
     /// Extends the <see cref="UnityEngine.Debug"/> class with additional functionality
     /// </summary>
-    public static class DebugHelper{
+    public static class DebugHelper {
         /// <summary>
         /// Draws a + at the specified point.
         /// </summary>
@@ -12,12 +12,43 @@ namespace StudioName.Runtime.ExtensionAndHelpers {
         /// <param name="duration">How long will the plus be displayed.</param>
         /// <param name="color">Color of our plus. Defaults to green.</param>
         /// <param name="size">The size of our plus.</param>
-        public static void DrawPoint(Vector3 point, float duration, Color? color = null, float size = 0.05f) {
+        public static void DrawPoint(Vector3 point, float duration, Color? color = null, float size = 0.05f, bool makeStar = false) {
             color = color ?? Color.green;
             Debug.DrawLine(point,point + new Vector3(size+0.01f,0,0),(Color)color, duration);
             Debug.DrawLine(point,point + new Vector3(-(size+0.01f),0,0),(Color)color, duration);
             Debug.DrawLine(point,point + new Vector3(0,size+0.01f,0),(Color)color, duration);
             Debug.DrawLine(point,point + new Vector3(0,-(size+0.01f),0),(Color)color, duration);
+            if (!makeStar) {
+                return;
+            }
+            Debug.DrawLine(point,point + new Vector3(size+0.01f,size+0.01f,0),(Color)color, duration);
+            Debug.DrawLine(point,point + new Vector3(-(size+0.01f),-(size+0.01f),0),(Color)color, duration);
+            Debug.DrawLine(point,point + new Vector3(-(size+0.01f),size+0.01f,0),(Color)color, duration);
+            Debug.DrawLine(point,point + new Vector3(size+0.01f,-(size+0.01f),0),(Color)color, duration);
+        }
+
+        /// <summary>
+        /// Draws a rectangle around the given bounds
+        /// </summary>
+        /// <param name="bounds">The bounds to draw a rectangle around.</param>
+        /// <param name="duration">How long will the plus be displayed.</param>
+        /// <param name="color">Color of our plus. Defaults to green.</param>
+        /// <param name="addX">Adds an x to the middle of the rectangle as well</param>
+        public static void DrawBound(Bounds bounds, float duration, Color? color = null, bool addX = false) {
+            var castedColor = color ?? Color.red;
+            var topLeft = new Vector2(bounds.min.x, bounds.max.y);
+            var topRight = bounds.max;
+            var bottomLeft = bounds.min;
+            var bottomRight = new Vector2(bounds.max.x, bounds.min.y);
+            Debug.DrawLine(topLeft,topRight,castedColor,100);
+            Debug.DrawLine(topRight,bottomRight,castedColor,100);
+            Debug.DrawLine(bottomRight,bottomLeft,castedColor,100);
+            Debug.DrawLine(bottomLeft,topLeft,castedColor,100);
+            if (!addX) {
+                return;
+            }
+            Debug.DrawLine(topLeft,bottomRight,castedColor,100);
+            Debug.DrawLine(topRight,bottomLeft,castedColor,100);
         }
     }
 }
